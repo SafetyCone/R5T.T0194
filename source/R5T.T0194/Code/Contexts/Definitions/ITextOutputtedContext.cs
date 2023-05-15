f0f26b35768e6b35de7940d2b81491255dbcc6cd
davidcoats <david@getsafetycone.com>
2023-05-14 23:45:35 -0700

    Interim changes.

---
+++ source/R5T.T0194/Code/Contexts/Definitions/ITextOutputtedContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.T0137;
+using R5T.T0159;
+
+
+namespace R5T.T0194
+{
+    /// <summary>
+    /// Attributed base context for contexts providing a text output.
+    /// </summary>
+    [ContextTypeMarker]
+    public interface ITextOutputtedContext : IContextDefinitionMarker
+    {
+        public ITextOutput TextOutput { get; }
+    }
+}
